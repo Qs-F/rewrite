f5ac47c64f65ca21e42773e50aac0eb1fb495419
Qs-F <qsf@de-liker.com>
2019-10-08 17:20:14 +0900

    Add docs

--- rewrite.go
+++ rewrite.go
@@ -8,13 +8,16 @@ import (
 	"github.com/Qs-F/bort"
 )
 
+// Replace is struct consisting of Old and New string
 type Replace struct {
 	Old string
 	New string
 }
 
+// Rule is slice of Replace
 type Rule []*Replace
 
+// Replacer returns strings.Replacer by converting from Rule to strings.Replacer
 func (rl *Rule) ToReplacer() *strings.Replacer {
 	replaces := []string{}
 	for _, rep := range *rl {
@@ -23,6 +26,8 @@ func (rl *Rule) ToReplacer() *strings.Replacer {
 	return strings.NewReplacer(replaces...)
 }
 
+// Rewrite is implementation of Rewriter.
+// If the content is binary, Rewrite returns original content.
 func (rl *Rule) Rewrite(p []byte) []byte {
 	if isbin, err := bort.IsBin(bytes.NewReader(p)); err != nil || isbin {
 		return p
@@ -30,6 +35,9 @@ func (rl *Rule) Rewrite(p []byte) []byte {
 	return []byte(rl.ToReplacer().Replace(string(p)))
 }
 
+// Map provides wrapper of http.Handler.
+// Map returns http.Handler.
+// By passing http.Handler, Map autonatically rewrite the content which handler write out.
 func (rl *Rule) Map(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rw := NewResponseWrite(w, rl)
@@ -37,6 +45,9 @@ func (rl *Rule) Map(h http.Handler) http.Handler {
 	})
 }
 
+// MapFunc provides wrapper of http.HandlerFunc.
+// MapFunc returns http.Handler.
+// By passing http.Handler, Map autonatically rewrite the content which handler write out.
 func (rl *Rule) MapFunc(hf http.HandlerFunc) http.Handler {
 	return rl.Map(hf)
 }
