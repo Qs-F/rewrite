98fa511ea19944e51ad18e878a9d0c01792cfcc9
Qs-F <qsf@de-liker.com>
2019-10-08 16:46:42 +0900

    Add tests

--- rewrite_test.go
+++ rewrite_test.go
@@ -1,1 +1,79 @@
 package rewrite
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type T struct {
+	Handler http.Handler
+	Rule    *Rule
+	Expect  string
+}
+
+func TestMap(t *testing.T) {
+	tests := []T{
+		{
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello Japan")
+			}),
+			Rule: &Rule{
+				{
+					Old: "Japan",
+					New: "World",
+				},
+			},
+			Expect: "Hello World",
+		},
+		{
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello Japan")
+			}),
+			Rule: &Rule{
+				{
+					Old: "World",
+					New: "Gopher",
+				},
+			},
+			Expect: "Hello Japan",
+		},
+		{
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello Japan")
+			}),
+			Rule: &Rule{
+				{
+					Old: "Japan",
+					New: "日本",
+				},
+			},
+			Expect: "Hello 日本",
+		},
+	}
+
+	for i, test := range tests {
+		srv := httptest.NewServer(test.Rule.Map(test.Handler))
+		resp, err := http.Get(srv.URL)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if !bytes.Equal(body, []byte(test.Expect)) {
+			t.Error(fmt.Sprintf("test %d: expect: %s but get: %s\n", i, test.Expect, string(body)))
+			continue
+		}
+		srv.Close()
+	}
+}
